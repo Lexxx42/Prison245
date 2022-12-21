c8e68869da291db70a5b815b3608645ba9e52f26
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-21 14:33:25 +0300

    test

--- prisoners.py
+++ prisoners.py
@@ -2,20 +2,9 @@ import json
 from Prison245.w_r_file import update_json
 from logger import logging
 from random import randint
+added code from Alexandra
 
 
-def print_prisoners(type_of_print):
-    useful = ''
-    if type_of_print == 'console':
-        for i in range(len(PRISONERS_LIST.get("prisoners"))):
-            useful += "id : " + PRISONERS_LIST.get("prisoners")[i]["id"] + '; ' + 'name : ' + PRISONERS_LIST.get(
-                "prisoners")[i]["name"] + '; ' + 'surname : ' + PRISONERS_LIST.get("prisoners")[i][
-                          "second_name"] + '; ' + 'block : ' + PRISONERS_LIST.get("prisoners")[i]["area"][
-                          "name"] + '; ' + 'cell : ' + PRISONERS_LIST.get("prisoners")[i]["area"][
-                          "cell"] + '; ' + 'reason : ' + PRISONERS_LIST.get("prisoners")[i][
-                          "reason"] + '; ' + 'status : ' + PRISONERS_LIST.get("prisoners")[i]["status"] + '; ' + '\n'
-        logging.info("data printed to console: prisoners")
-        print(useful)
     elif type_of_print == 'file':
         for i in range(len(PRISONERS_LIST.get("prisoners"))):
             useful += "id : " + PRISONERS_LIST.get("prisoners")[i]["id"] + '; ' + 'name : ' + PRISONERS_LIST.get(
