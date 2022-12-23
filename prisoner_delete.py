1938aa6d912dc03081d1af1bf5a633aa6013409e
NDania <113174899+NDania@users.noreply.github.com>
2022-12-23 21:06:17 +0300

    Delete prisoner_delete.py
    Unworking module

--- prisoner_delete.py
+++
@@ -1,7 +0,0 @@
-def pr_1(k,m):
-  new_spis = k
-  for i in range(len(new_spis["prisoners"])):
-    if new_spis["prisoners"][i]["id"] == m:
-      new_spis["prisoners"][i]["status"] = k["prisoners"][i]["status"].replace("in jail","free")
-  
-  return new_spis 
