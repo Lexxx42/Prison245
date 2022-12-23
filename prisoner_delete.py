8acef19ea88fa2ddf5629d9ea57756bef2dd360c
Dania <Dania2008@list.ru>
2022-12-23 21:21:05 +0300

    Updated moduls with delete

--- prisoner_delete.py
+++ prisoner_delete.py
@@ -1,7 +1,10 @@
-def pr_1(k,m):
-  new_spis = k
-  for i in range(len(new_spis["prisoners"])):
-    if new_spis["prisoners"][i]["id"] == m:
-      new_spis["prisoners"][i]["status"] = k["prisoners"][i]["status"].replace("in jail","free")
-  
-  return new_spis 
+def delete_pris(PRISONERS_LIST:dict, id_pris:int):
+  new_spis = PRISONERS_LIST
+  for i in range(len(new_spis.get("prisoners"))):
+    if int(new_spis.get("prisoners")[i]["id"]) == id_pris:
+      new_spis["prisoners"][i]["status"] = PRISONERS_LIST["prisoners"][i]["status"].replace("in jail","free")
+  return new_spis  
+
+
+
+         
