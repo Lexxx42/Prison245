4d651e384e3940ed5ef65880ef8b92b195ef8401
NDania <113174899+NDania@users.noreply.github.com>
2022-12-23 21:05:22 +0300

    Delete employees_delete.py
    unworking modul

--- employees_delete.py
+++
@@ -1,7 +0,0 @@
-def pr_1(k,m):
-  new_spis = k
-  for i in range(len(new_spis["employees"])):
-    if new_spis["employees"][i]["id"] == m:
-      new_spis["employees"][i]["status"] = k["employees"][i]["status"].replace("working","fired")
-  
-  return new_spis 
